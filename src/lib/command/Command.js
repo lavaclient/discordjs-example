5f014174add6a2697d78c5b6ae1b1b05d86031da
Seren_Modz 21 <seren@kings-world.net>
2021-08-22 05:03:08 +0100

    :fire: remove command decorator

--- src/lib/command/Command.js
+++ src/lib/command/Command.js
@@ -1,13 +1,3 @@
-export function command(data) {
-    return (target) => {
-        return class extends target {
-            constructor(...args) {
-                super(data, ...args);
-            }
-        }
-    }
-}
-
 export class Command {
     constructor(data) {
         this.data = data;
